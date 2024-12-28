d0c620c127dce6da3c36ad54b3f300ea0bd1ff2f
Durbek <saydaliyevdurbek0512@gmail.com>
2024-12-29 00:06:08 +0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  wsUrl: 'http://localhost:3000',
-  // wsUrl: 'https://tic-tac-toe-client-8c08.onrender.com',
+  // wsUrl: 'http://localhost:3000',
+  wsUrl: 'https://tic-tac-toe-client-8c08.onrender.com',
 };
