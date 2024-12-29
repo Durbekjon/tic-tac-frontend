f5f10003a75410fd7d501fdbf45697f956971462
Durbek <saydaliyevdurbek0512@gmail.com>
2024-12-30 02:45:28 +0500

    backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   wsUrl: 'wss://tic-tac-toe-client-8c08.onrender.com',
 
-  apiUrl: 'http://tic-tac-toe-client-8c08.onrender.com/api',
+  apiUrl: 'http://194.61.28.102:3000/api',
   appKey: '6920f11c-1827-474b-be85-81d3fad2c057',
 };
