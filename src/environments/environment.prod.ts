4018898878cb21396fa2d0d9032e0c9ac89ee701
Durbek <saydaliyevdurbek0512@gmail.com>
2025-01-27 13:05:25 +0500

    invironment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  wsUrl: 'wss://app2.koproq.uz',
+  wsUrl: 'wss://tic-tac-toe-client-8c08.onrender.com',
 
-  apiUrl: 'https://app2.koproq.uz/api',
+  apiUrl: 'https://tic-tac-toe-client-8c08.onrender.com/api',
   appKey: '6920f11c-1827-474b-be85-81d3fad2c057',
 };
